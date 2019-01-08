8bd249f60ebdeed53d6a28a549f475f32d3783a9
Tejpal <tejpal.borkar@coreviewsystems.com>
2019-01-08 18:54:21 +0530

    spring jdbc with maven

---
+++ spring-boot-jdbc-template/src/main/java/com/assimilate/spring/SpringBootJdbcTemplateApplication.java
@@ -0,0 +1,15 @@
+package com.assimilate.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+//@SpringBootApplication
+public class SpringBootJdbcTemplateApplication {
+
+	public static void main(String[] args) {
+//		ApplicationContext
+	}
+
+}
+
---
+++ spring-boot-jdbc-template/src/test/java/com/assimilate/spring/SpringBootJdbcTemplateApplicationTests.java
@@ -0,0 +1,17 @@
+package com.assimilate.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootJdbcTemplateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
